0c905e5cce0621fc54b556723e5731b4899b4f19
changwoo <soup1234567@gmail.com>
2018-09-05 01:32:14 +0900

    add pyhon_exercise(if_else etc.)

---
+++ python_intro.py
@@ -0,0 +1,34 @@
+print('Hello, Django')
+
+if 3 > 2:
+    print('It works!')
+
+if 5 > 2:
+    print('5 is indeed greater than 2')
+else:
+    print('5 is not greater than 2')
+
+
+name = 'Sonja'
+if name == 'Ola':
+    print('Hey Ola!')
+elif name == 'Sonja':
+    print('Hey Sonja!')
+else:
+    print('Hey anonymous!')
+
+
+# volume 값을 바꿔보세요
+volume = 57
+if volume < 20:
+    print("It's kinda quiet.")
+elif 20 <= volume < 40:
+    print("It's nice for background music")
+elif 40 <= volume < 60:
+    print("Perfect, I can hear all the details")
+elif 60 <= volume < 80:
+    print("Nice for parties")
+elif 80 <= volume < 100:
+    print("A bit loud!")
+else:
+    print("My ears are hurting! :(")
